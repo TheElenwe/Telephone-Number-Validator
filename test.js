d2f1614d2b06e874baaf72fec72a7f4057784022
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-08-03 20:24:53 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+function telephoneCheck(str,meme) {
+  
+  
+    
+     if(str.length ===4){
+        return true;
+    }
+  
+  else if (str ==="1"){
+    return true
+  }
+   else {
+       return false;
+   }
+    
+  }
+  
+  console.log(telephoneCheck("12222"));
